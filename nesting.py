834d9c260d81f7c742a69f280b81fb627585e803
Adam Dominec <adominec@gmail.com>
2023-04-30 13:47:02 +0200

    WIP bitmap cross-correlation packing

--- nesting.py
+++ nesting.py
@@ -14,8 +14,24 @@ class Page:
         self.image_path = None
 
 
+def can_pack_ccor(islands, cage_size):
+    if len(islands) <= 1:
+        islands[0].bounding_box.xy = 0, 0
+        return True
+    for isle in islands[1:]:
+        ...
+    boxes = [[0, 0, isle.bounding_box.x, isle.bounding_box.y / aspect] for isle in islands]
+    width, height = box_pack_2d(boxes)
+    if width < cage_size.x > height:
+        for (x, y, *_), isle in zip(boxes, islands):
+            isle.pos.xy = x, y * aspect
+        return True
+    return False
+
+
 def can_pack_bpy(islands, cage_size):
     if len(islands) <= 1:
+        islands[0].bounding_box.xy = 0, 0
         return True
     aspect = cage_size.y / cage_size.x
     boxes = [[0, 0, isle.bounding_box.x, isle.bounding_box.y / aspect] for isle in islands]
@@ -30,10 +46,10 @@ def can_pack_bpy(islands, cage_size):
 def branch_and_bound(can_pack_fn):
     # exhaustive search for minimal number of pages
     def result(islands, cage_size):
-        best = [i for i, _ in enumerate(islands)]
+        best = [(i, [0, 0]) for i, _ in enumerate(islands)]
         path = [0]
         while True:
-            if path[-1] > max(path[:-1], default=0) + 1 or max(path) >= max(best):
+            if path[-1] > max(path[:-1], default=0) + 1 or max(path) >= max(i for i, _ in best):
                 # invalid step
                 path.pop()
                 if len(path) <= 1:
@@ -44,7 +60,7 @@ def branch_and_bound(can_pack_fn):
             if can_pack_fn(page, cage_size):
                 # good step
                 if len(path) == len(islands):
-                    best = list(path)
+                    best = [(i, isle.pos.xy) for i, isle in zip(path, islands)]
                     path.pop()
                     path[-1] += 1
                 else:
@@ -52,11 +68,10 @@ def branch_and_bound(can_pack_fn):
             else:
                 # bad step
                 path[-1] += 1
-        pages = [list() for _ in range(max(best) + 1)]
-        for i, isle in zip(best, islands):
+        pages = [list() for _ in range(max(i for i, _ in best) + 1)]
+        for (i, pos), isle in zip(best, islands):
+            isle.pos.xy = pos
             pages[i].append(isle)
-        for page in pages:
-            can_pack_fn(page, cage_size)
         return [Page(i, page) for i, page in enumerate(pages)]
     return result
 
